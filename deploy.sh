b8488c8510378c1c4339be53b38c34221657c7b7
Rodick.Huang <rodick@ehanlin.com.tw>
2018-05-24 12:14:32 +0800

    about #3740 加入廣告連結

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-shreview\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"shreview\"}" 'https://www.ehanlin.com.tw/event/api/Deploy'
+#!/bin/sh
+case $TRAVIS_TAG in
+  *-SNAPSHOT ) API_HOST='test.ehanlin.com.tw';;
+  * ) API_HOST='www.ehanlin.com.tw';;
+esac
+
+echo "TRAVIS_TAG => [$TRAVIS_TAG]"
+echo "API_HOST => [$API_HOST]"
+echo $API_HOST
+curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-shreview\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"shreview\"}" "https://${API_HOST}/event/api/Deploy"
